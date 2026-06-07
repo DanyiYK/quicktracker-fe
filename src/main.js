f76d45c80beef8ece68a55def25c6675012365d8
Den <danielranieri06@gmail.com>
2026-06-07 15:25:38 +0200

    feat: add router

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 import "./style.css";
+import { defineRoutes } from "@just-dom/router";
 import { createRoot } from "just-dom";
 import { jd } from "./jd.config.js";
+
+
+const routes = defineRoutes([
+  { path: '/login', element: ()=>{return document.createTextNode("Login page")} },
+  { path: '', element: ()=>{return document.createTextNode("Not found")} }
+])
+
+createRoot(
+  "app",
+  jd.router(routes)
+);
