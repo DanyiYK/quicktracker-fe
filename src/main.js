d7fd692063ee9563293f2950e8a277fec04572a0
Den <danielranieri06@gmail.com>
2026-06-08 09:32:06 +0200

    fix: spelling error in imports

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import { defineRoutes } from "@just-dom/router";
 import { createRoot } from "just-dom";
 import { jd } from "./jd.config.js";
 import { NotFound } from "./pages/not-found-page.js";
-import { BaseLayout } from "./layout/base-layout.js";
+import { BaseLayout } from "./layouts/base-layout.js";
+import { LoginForm } from "./pages/login-page.js";
 
 const routes = defineRoutes([
-  { path: '/login', element: ()=>{return document.createTextNode("Login page")} },
+  { path: '/login', element: ()=>{return BaseLayout({ content: LoginForm() })} },
   { path: '*', element: ()=>{ return BaseLayout({ content: NotFound() }) } }
 ])
 
