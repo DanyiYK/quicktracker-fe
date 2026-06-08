0b59b151e7d80f28a5be78d28c5e04c53a918636
Den <danielranieri06@gmail.com>
2026-06-08 10:37:33 +0200

    refactor: rename pages functions

--- src/main.js
+++ src/main.js
@@ -2,13 +2,14 @@ import "./style.css";
 import { defineRoutes } from "@just-dom/router";
 import { createRoot } from "just-dom";
 import { jd } from "./jd.config.js";
-import { NotFound } from "./pages/not-found-page.js";
+import { NotFound as NotFoundPage } from "./pages/not-found-page.js";
 import { BaseLayout } from "./layouts/base-layout.js";
-import { LoginForm } from "./pages/login-page.js";
+import { LoginPage } from "./pages/login-page.js";
+import { DashboardPage } from "./pages/dashboard-page.js";
 
 const routes = defineRoutes([
-  { path: '/login', element: ()=>{return BaseLayout({ content: LoginForm() })} },
-  { path: '*', element: ()=>{ return BaseLayout({ content: NotFound() }) } }
+  { path: '/login', element: ()=>{return BaseLayout({ content: LoginPage() })} },
+  { path: '*', element: ()=>{ return BaseLayout({ content: NotFoundPage() }) } }
 ])
 
 createRoot(
