3f41694dfc9f3608010ac9b20acd77743d0233e5
Den <danielranieri06@gmail.com>
2026-06-08 21:36:51 +0200

    feat: add dashboard sidebar options

--- src/layouts/dashboard-layout.js
+++ src/layouts/dashboard-layout.js
@@ -1,8 +1,28 @@
+import { navigate } from "@just-dom/router";
 import { Header } from "../components/header";
+import { VITE_API_URL } from "../env";
 import { jd } from "../jd.config";
 
 export function DashboardLayout(props) {
     const { headerData = {}, content = jd.fragment(), options = [] } = props;
+
+    const token = localStorage.getItem('token');
+
+    if(!token) {
+        navigate('/login');
+    } else {
+        fetch(`${VITE_API_URL}/auth/me`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res=>{
+            if(res.ok){return}
+            localStorage.removeItem('token');
+            document.location.href = '/login';
+        })
+    }
 
     return jd.fragment([
         Header(headerData),
@@ -17,7 +37,7 @@ export function DashboardLayout(props) {
                 jd.ul({ className: "menu bg-base-200 min-h-full w-64 p-4" }, options.map(value => {
                     const { text = '', href = '#', icon = 'FileQuestion' } = value;
 
-                    return jd.li({}, [jd.a({ href: href, className: `w-full mb-1 px-0 ${document.location.pathname.startsWith(href)?'bg-primary text-primary-content':''}` }, [jd.lucide(icon, { size: 22, className: 'ml-2' }), text])])
+                    return jd.li({}, [jd.a({ href: href, className: `w-full mb-1 px-0 ${document.location.pathname.startsWith(href) ? 'bg-primary text-primary-content' : ''}` }, [jd.lucide(icon, { size: 22, className: 'ml-2' }), text])])
                 })),
             ]),
         ])
--- src/main.js
+++ src/main.js
@@ -2,17 +2,40 @@ import "./style.css";
 import { defineRoutes } from "@just-dom/router";
 import { createRoot } from "just-dom";
 import { jd } from "./jd.config.js";
-import { NotFound as NotFoundPage } from "./pages/not-found-page.js";
+import { NotFoundPage } from "./pages/not-found-page.js";
 import { BaseLayout } from "./layouts/base-layout.js";
 import { LoginPage } from "./pages/login-page.js";
-import { DashboardPage } from "./pages/dashboard-page.js";
+import { DashboardLayout } from "./layouts/dashboard-layout.js";
 
 const routes = defineRoutes([
-  { path: '/login', element: ()=>{return BaseLayout({ content: LoginPage() })} },
-  { path: '*', element: ()=>{ return BaseLayout({ content: NotFoundPage() }) } }
-])
+    { path: '/login', element: () => { return BaseLayout({ content: LoginPage() }) } },
+    {
+        path: '/dashboard/:section', element: () => {
+            return DashboardLayout({
+                options: [
+                    {
+                        text: 'Deliveries',
+                        icon: 'Box',
+                        href: '/dashboard/deliveries'
+                    },
+                    {
+                        text: 'Couriers',
+                        icon: 'Truck',
+                        href: '/dashboard/couriers'
+                    },
+                    {
+                        text: 'Statistics',
+                        icon: 'GitGraph',
+                        href: '/dashboard/statistics'
+                    },
+                ]
+            })
+        }
+    },
+    { path: '*', element: () => { return BaseLayout({ content: NotFoundPage() }) } }
+]);
 
 createRoot(
-  "app",
-  jd.router(routes)
+    "app",
+    jd.router(routes)
 );
